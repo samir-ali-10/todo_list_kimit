7a579e2158810bb4986cd0cb97f9dea84e370975
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-03-27 00:50:28 +0200

    not ready yet

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let inputTask = document.getElementById("input_task"),
+    btn = document.querySelector(".btn"),
+    outputTask = document.querySelector("ol");
+
+
+btn.onclick = function () {
+    if(inputTask.value == " ") {
+        alert("Input Invalid");
+    }
+    else {
+        outputTask.innerHTML += `<li>${inputTask.value}</li>`;
+        inputTask.value = " ";
+        inputTask.focus();
+    }
+}
